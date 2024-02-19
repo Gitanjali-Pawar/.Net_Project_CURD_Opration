81791adaab3ec62f3a59d5b09fe98ca719f6e1e2
Gitanjali-Pawar <pawargitanjali2000@gmail.com>
2024-02-19 20:09:21 +0530

    first commit

---
+++ EmployeeData/Models/EmployeeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeData.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName(" Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName(" Date Of Birth")]
+
+        public DateTime DateOfBirth { get; set; }
+
+        [DisplayName(" E-mail")]
+        public string Email { get; set; }
+
+        [DisplayName(" Salary")]
+        public double Salary { get; set; }
+
+        [DisplayName(" Name")]
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }   
+        }
+
+
+    }
+}
